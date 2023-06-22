a95f98fe0a54a8d094c4d78c09a05afa2db98637
ehsansdumb <127270636+ehsansdumb@users.noreply.github.com>
2023-06-22 20:13:49 +0400

    Update registration.js

--- 128 Prj/registration.js
+++ 128 Prj/registration.js
@@ -1,46 +1,65 @@
 function showPass1() {
-
-    var x = document.getElementById("new_password");
-    if (x.type === "password") {
+  var x = document.getElementById("new_password");
+  if (x.type === "password") {
     x.type = "text";
-
-    } else {
-        x.type = "password";
-    }
-} 
-function showPass2() {
-
-    var x = document.getElementById("repeated_password");
-    if (x.type === "password") {
-    x.type = "text";
-
-    } else {
-        x.type = "password";
-    }
-} 
-
-function passmatch(){
-    var x = document.getElementById("new_password").value;
-    
-    var y = document.getElementById("repeated_password").value;
-    
-    var messageElement = document.getElementById("message");
-
-    if(x == y){
-        
-        messageElement.textContent = "Registered Successfully.";
-
-        let username = document.getElementById("new_username").value;
-        let password = document.getElementById("new_password").value;
-
-//localstorage
-        localStorage.setItem('username', username);
-        localStorage.setItem('password', password);
-
-        alert("Details saved in local storage");
-    }
-    else{
-        messageElement.textContent = "Fields do not match. Please try again.";
-        document.getElementById("repeated_password").value = "";
-    }
+  } else {
+    x.type = "password";
+  }
 }
+function showPass2() {
+  var x = document.getElementById("repeated_password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function passmatch() {
+  var x = document.getElementById("new_password").value;
+  var y = document.getElementById("repeated_password").value;
+  
+  var messageElement = document.getElementById("message");
+  
+  if (x === y) {
+    var firstname = document.getElementById("fname").value;
+    var lastname = document.getElementById("lname").value;
+    var username = document.getElementById("new_username").value;
+    var email = document.getElementById("new_email").value;
+    var password = document.getElementById("new_password").value;
+  
+    // Create an object with the user data
+    var userData = {
+      firstname: firstname,
+      lastname: lastname,
+      username: username,
+      email: email,
+      password: password
+    };
+  
+    // Make a POST request to the server-side API
+    fetch('/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.message);
+        messageElement.textContent = "Registered Successfully.";
+        alert("User created");
+      })
+      .catch(error => {
+        console.error('Error registering the user:', error);
+        messageElement.textContent = "An error occurred while registering. Please try again.";
+      });
+  } else {
+    messageElement.textContent = "Fields do not match. Please try again.";
+    document.getElementById("repeated_password").value = "";
+  }
+}
+
+
+
