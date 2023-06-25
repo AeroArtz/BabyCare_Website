5b0d8c2e851300ea88ed7c8d2dbf007c242ecdf6
sabihhuda <136050361+sabihhuda@users.noreply.github.com>
2023-06-25 14:29:15 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -34,3 +34,42 @@ const link = document.createElement('a');
 link.href = url;
 link.download = filename;
 link.click();
+
+let checkoutItems = [];
+let totalPrice = 0;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const addButtons = document.getElementsByClassName('add');
+    for (let i = 0; i < addButtons.length; i++) {
+        addButtons[i].addEventListener('click', addToCheckout);
+    }
+});
+
+function addToCheckout(event) {
+    const serviceElement = event.target.parentNode;
+    const serviceName = serviceElement.querySelector('h2').innerText;
+    const price = getPrice(serviceElement.querySelector('.price').innerText);
+
+    checkoutItems.push({ serviceName, price });
+    totalPrice += price;
+
+    updateCheckout();
+}
+
+function updateCheckout() {
+    const checkoutList = document.getElementById('checkout-list');
+    const totalPriceElement = document.getElementById('total-price');
+
+    checkoutList.innerHTML = '';
+    checkoutItems.forEach((item) => {
+        const li = document.createElement('li');
+        li.innerText = `${item.serviceName} - AED ${item.price}`;
+        checkoutList.appendChild(li);
+    });
+
+    totalPriceElement.innerText = 'Total: AED ' + totalPrice;
+}
+
+function getPrice(priceString) {
+    return parseInt(priceString.replace('AED ', ''), 10);
+}
