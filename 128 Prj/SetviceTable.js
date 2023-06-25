bd8bba7b3c096a8520e3d58ca4fdd5a4d1f50fdf
axus1st <30731993+axus1st@users.noreply.github.com>
2023-06-25 13:59:54 +0400

    Add files via upload

---
+++ 128 Prj/SetviceTable.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin", 
+    database: "blossom_care" 
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    
+    var sql_table = "CREATE TABLE services (service_id INT PRIMARY KEY, " + 
+    "service_name VARCHAR(255)," +
+    "service_name VARCHAR(255)," +
+    "price DECIMAL(10, 2))" ;
+    con.query(sql_table, function (err, result) {
+        if (err) throw err;
+        console.log("Service Table created");
+    });
+});
